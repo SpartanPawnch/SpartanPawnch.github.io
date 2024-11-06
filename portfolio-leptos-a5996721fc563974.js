5a97eb165cea56030b21cf5d9ddbd183226aa9ec
Martin Tsvetkov <marti@tsvetkov.eu.com>
2024-11-06 13:48:53 +0000

    Deploy 2024-11-06T13:39:36

--- portfolio-leptos-a5996721fc563974.js
+++ portfolio-leptos-a5996721fc563974.js
@@ -694,7 +694,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('leptos-tutorial_bg.wasm', import.meta.url);
+        module_or_path = new URL('portfolio-leptos_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
